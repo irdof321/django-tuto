d218b5b30d68d5251a930e5706a12ae7162f88f6
David <david.forgione@syrto.ch>
2023-05-15 16:07:15 +0200

    UPDATE

--- Exo2/src/blog/models.py
+++ Exo2/src/blog/models.py
@@ -27,3 +27,43 @@ class BlogPost(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Author(models.Model):
+    firstname = models.CharField(max_length=35,blank=False)
+    lastname = models.CharField(max_length=35,blank=False)
+    wikipedia = models.URLField(blank=True,null=True)
+    
+    def __str__(self):
+        return "{} {}".format(self.firstname,self.lastname)
+    
+class Book(models.Model):
+    Aventure = 'AV'
+    Thriller = 'TH'
+    Romance = 'RO'
+    Fantastique = 'FA'
+    Horreur = 'HO'
+    ScienceFiction = 'SF'
+    Category = [
+        (Aventure,'Aventure'),
+        (Thriller,'Thriller'),
+        (Romance,'Romance'),
+        (Fantastique,'Fantastique'),
+        (Horreur,'Horreur'),
+        (ScienceFiction,'Science-Fiction'),
+    ]
+    
+    
+    title = models.CharField(max_length=35,blank=False)
+    price = models.DecimalField(max_digits=5,decimal_places=2,blank=True,null=True)
+    summary = models.TextField(blank=True,null=True)
+    author = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    category = models.CharField(max_length=2,choices=Category,blank=True,null=True)
+    stock = models.IntegerField(default=0)
+    
+    def save(self,*args,**kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args,**kwargs)
+
+    def __str__(self):
+        return self.title
